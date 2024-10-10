5aa0001f4c3a1538f387525d7e17865f09e6c34d
Sagar Dhadke <peaceunique2003@gmail.com>
2024-10-10 12:07:16 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/chetan_bhagat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chetan_bhagat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomeScreen extends StatelessWidget {
+  const MyHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chetan Bhagat"),
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
